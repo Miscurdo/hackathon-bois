c3627d52c9d921fb2e0e3f46926e7c1177374a60
Marco Seidenberg <marco.seidenberg@student.unsw.edu.au>
2021-07-17 19:57:16 +1000

    Changes to lecturer/student

--- Lecturer.py
+++ Lecturer.py
@@ -1,18 +1,20 @@
+import data
+import Student
 ###
 # Set up a new course - initialise with
 # password
 def addCourse(courseCode, password):
-    courses.add(courseCode, password)
+    data.courses.add(courseCode, password)
 
 #
 def addQuestion(Question, courseCode):
-    for course in courses:
+    for course in data.courses:
         if courseCode == course.courseCode:
             course.questions.add(Question)
 
 # Adds a student to an available course
 def enrollStudent(Student, courseCode):
-    for course in courses:
+    for course in data.courses:
         if courseCode == course.courseCode:
             password = course.password
 
--- Student.py
+++ Student.py
@@ -1,15 +1,18 @@
+import data
 ###
+questionList = []
+
 # Adds a student to course with corresponding courseCode
 # automatically adds all questions associated to that course to student
 def joinCourse(courseCode, password):
-    for course in courses:
+    for course in data.courses:
         if courseCode == course.courseCode and course.password == password:
             for question in course.questions:
-                self.questions.add(question)
+                questionList.add(question)
 
 # Removes student from course
 # removes all associated questions from that student
 def removeCourse(courseCode):
-    for question in self.questions:
+    for question in questionList:
         if question.courseCode == courseCode:
-            self.qeustions.remove(courseCode)
+            questionList.remove(question)
