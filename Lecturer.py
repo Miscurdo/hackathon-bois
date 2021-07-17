5e434ed3f178b0b1cdc199d502f50343080ed83b
Jamie <john.conlon@student.unsw.edu.au>
2021-07-17 23:53:03 +1000

    TODO added

--- Lecturer.py
+++ Lecturer.py
@@ -6,7 +6,7 @@ import Student
 def addCourse(courseCode, password):
     data.courses.append({"courseCode": courseCode, "coursePin": password, "questionList": []})
 
-#
+# TODO: Should go through all students enrolled in the course and add the question to their list of questions.
 def addQuestion(Question, courseCode):
     for course in data.courses:
         if courseCode == course['courseCode']:
