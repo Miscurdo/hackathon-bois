793e1fa279d1d228aa97138fddcb1ab5a0d85273
Marco Seidenberg <marco.seidenberg@student.unsw.edu.au>
2021-07-17 20:19:59 +1000

    changes to Student/Lecturer

--- Student.py
+++ Student.py
@@ -1,15 +1,26 @@
+import data
+import auth
 ###
+
 # Adds a student to course with corresponding courseCode
 # automatically adds all questions associated to that course to student
-def joinCourse(courseCode, password):
-    for course in courses:
+def joinCourse(courseCode, password, token):
+    userID = auth.authenticate(token)
+    if (auth.authenticate(token) == None): return
+    user = data.users[userID]
+
+    for course in data.courses:
         if courseCode == course.courseCode and course.password == password:
             for question in course.questions:
-                self.questions.add(question)
+                user.questionList.add(question)
 
 # Removes student from course
 # removes all associated questions from that student
-def removeCourse(courseCode):
-    for question in self.questions:
+def removeCourse(courseCode, token):
+    userID = auth.authenticate(token)
+    if (auth.authenticate(token) == None): return
+    user = data.users[userID]
+
+    for question in user.questionList:
         if question.courseCode == courseCode:
-            self.qeustions.remove(courseCode)
+            user.questionList.remove(courseCode)
