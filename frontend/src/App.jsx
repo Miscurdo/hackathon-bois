f88cd5f31c37d50fa06eb17d65e39078b6d767c2
annie <z5209781@unsw.edu.au>
2021-07-18 01:36:08 +1000

    added authentication pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
-// import LoginPage from './pages/Login';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
 import TitlePage from './pages/Title';
+import ForgotPasswordPage from './pages/ForgotPassword';
 
 const history = createBrowserHistory();
 function App() {
@@ -14,6 +16,15 @@ function App() {
         <Switch>
           <Route exact path="/">
             <TitlePage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/register">
+            <RegisterPage />
+          </Route>
+          <Route exact path="/forgot-password">
+            <ForgotPasswordPage />
           </Route>
         </Switch>
       </Router>
--- frontend/src/pages/Title.jsx
+++ frontend/src/pages/Title.jsx
@@ -1,39 +1,82 @@
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Button, Grid, Paper, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import logo from '../logo.png';
+
+import '../App.css';
+
+const backend = `http://localhost:4000`;
 
 const useStyles = makeStyles({
   form: {
     padding: '20px',
     height: '70vh',
     width: 400,
-    margin: '30px auto',
+    margin: '80px auto',
     textAlign: 'center'
-  }
+  },
+  logo: {
+    display: 'block',
+    margin: '50px auto',
+    height: 'auto',
+    width: '150px',
+  },
+  buttonDiv: {
+    flexDirection: 'column'
+  },
+  button: {
+    color: 'white',
+    backgroundColor: "#731DD8",
+    '&:hover': {
+      backgroundColor: '#C8B8D8',
+      color: 'black',
+    },
+    margin: '60px auto 0 auto',
+    display:'block',
+  },
 });
 
 const TitlePage = () => {
   const classes = useStyles();
+  const history = useHistory();
+  const handleLoginRedirect = (e) => {
+    history.push(`./login`);
+  };
+
+  const handleRegisterRedirect = (e) => {
+    history.push(`./register`);
+  };
 
   return (
     <div className="login-register-paper">
-      Hello World
       <Grid>
         <Paper elevation={10} className={classes.form}>
-          <img alt="Requizit Logo" src="../../public/logo.png">
-          </img>
-          <Button 
+          <img alt="Requizit Logo" src={logo} className={classes.logo}></img>
+          <Typography 
             style={{
-              backgroundColor: "#731DD8",
-              color: 'white'
+              fontWeight: 'bold',
+              fontFamily: 'Bree Serif'
             }}
-            variant="contained" >Login
-          </Button>
-          <Typography>
-            Don't have an account yet? Sign up now
+            variant="h4">
+            Requizit
           </Typography>
-          <Button variant="contained" color="primary">Sign up</Button>
+          <Typography 
+            style={{
+              marginTop: "10px",
+              fontFamily: "Bree Serif"
+            }}
+            variant="h5">
+            Review. Revisit. Requizit
+          </Typography>
+          <div className={classes.buttonDiv}>
+            <Button className={classes.button} variant="contained" onClick={handleLoginRedirect}>
+              Login
+            </Button>
+            <Button variant="contained" style={{marginTop: '10px'}} className={classes.button} onClick={handleRegisterRedirect}>
+              Sign Up
+            </Button>
+          </div>
         </Paper>
       </Grid>
     </div>
