25e1f97689f2ca700d1f6bc7dec3e05283686b43
Marco Seidenberg <marco.seidenberg@student.unsw.edu.au>
2021-07-17 18:54:50 +1000

    add student/lecturer draft

--- Lecturer.py
+++ Lecturer.py
@@ -1,14 +1,19 @@
 ###
 # Set up a new course - initialise with
 # password
-def addCourse(courseName, password):
-    pass
+def addCourse(courseCode, password):
+    courses.add(courseCode, password)
 
 #
 def addQuestion(Question, courseCode):
-    pass
+    for course in courses:
+        if courseCode == course.courseCode:
+            course.questions.add(Question)
 
 # Adds a student to an available course
-def enrollStudent(Student, courseCode)
+def enrollStudent(Student, courseCode):
+    for course in courses:
+        if courseCode == course.courseCode:
+            password = course.password
+
     Student.joinCourse(courseCode, password)
-    pass
--- Student.py
+++ Student.py
@@ -2,9 +2,14 @@
 # Adds a student to course with corresponding courseCode
 # automatically adds all questions associated to that course to student
 def joinCourse(courseCode, password):
-    pass
+    for course in courses:
+        if courseCode == course.courseCode and course.password == password:
+            for question in course.questions:
+                self.questions.add(question)
 
 # Removes student from course
 # removes all associated questions from that student
 def removeCourse(courseCode):
-    pass
+    for question in self.questions:
+        if question.courseCode == courseCode:
+            self.qeustions.remove(courseCode)
