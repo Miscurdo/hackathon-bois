dbc32b7248156a0c29c4d815fc172fca677ca540
Jamie <john.conlon@student.unsw.edu.au>
2021-07-17 19:04:16 +1000

    data.py can now load from a file

--- data.py
+++ data.py
@@ -1,11 +1,26 @@
+import json
 ###
 # loads info at server startup
+# To access list of courses or list of users import data at the top of you py file.
+# Then the list can be accessed by data.courses or data.users.
 ###
 def init():
-    global users = []
-    global classes = []
+    f = open("classes/courses", "r")
+    userJSON = f.read()
+    f.close()
+
+    f = open("users/users", "r")
+    courseJSON = f.read()
+    f.close()
+
+    global users = json.loads(userJSON)
+    global courses = json.loads(courseJSON)
 
 # Saves all current values for users and classes to their respective JSON files.
+# To be completed later since we dont have to save at this point.
+# TODO.
 def saveData():
-    pass
+    # Ensure all users are logged out before server shutdown.
+    for user in users:
+        user['token'] = []
 
