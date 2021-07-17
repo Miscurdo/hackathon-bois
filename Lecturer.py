e3e59d9192a859bbb59986a0cc6c469c6608a8fd
Marco Seidenberg <marco.seidenberg@student.unsw.edu.au>
2021-07-17 17:10:41 +1000

    added student/lecturer

---
+++ Lecturer.py
@@ -0,0 +1,14 @@
+###
+# Set up a new course - initialise with
+# password
+def addCourse(courseName, password):
+    pass
+
+#
+def addQuestion(Question, courseCode):
+    pass
+
+# Adds a student to an available course
+def enrollStudent(Student, courseCode)
+    Student.joinCourse(courseCode, password)
+    pass
---
+++ Student.py
@@ -0,0 +1,10 @@
+###
+# Adds a student to course with corresponding courseCode
+# automatically adds all questions associated to that course to student
+def joinCourse(courseCode, password):
+    pass
+
+# Removes student from course
+# removes all associated questions from that student
+def removeCourse(courseCode):
+    pass
